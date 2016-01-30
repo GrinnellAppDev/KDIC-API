5b1b40add77d7845221075910ead8b06bce8b0f1
shaun117 <shaun.mataire@live.com>
2016-01-30 14:58:42 -0600

    init setup

---
+++ routes/albumReviews.js
@@ -0,0 +1,75 @@
+//
+//  albumReviews.js
+//  kdicAPI
+//
+//  Created by Shaun Mataire on 1/14/16.
+//  Copyright © 2016 AppDev. All rights reserved.
+//
+
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var cheerio = require('cheerio');
+
+/* GET  albumReviews. */
+router.get('/', function(req, res, next) {
+
+  var albumReviewsUrl = 'http://kdic.grinnell.edu/?cat=6'
+
+  request(albumReviewsUrl, function(error, response, html){
+    //check whether there was an error making the request
+    if(!error){
+      //get the cheerio html object
+      var $ = cheerio.load(html);
+
+      //json result = articles object that is an array of json objects
+      var blogsJson = {
+        "articles": []
+      }
+
+      //we'' use the unique header as a starting point
+      $('div .posts-layout').filter(function(){
+
+        //get the html object data
+        var data = $(this)
+
+        //loop through articles
+        data.children().each(function(){
+
+          //varticle objects
+          var articleTitle, fullArticleUrl, articleText, datePosted, author, imageUrl, youtubeUrl;
+
+          //artcle object
+          var articleJson = {
+            articleTitle: "",
+            fullArticleUrl: "",
+            articleText: "",
+            datePosted: "",
+            author: "",
+            imageUrl: "",
+            youtubeUrl: ""
+          }
+
+          $(this).each(function(){
+
+            articleJson.articleTitle = $(this).find('.entry-header').first().children().first().text();
+            articleJson.fullArticleUrl = 'nil';
+            articleJson.articleText = $(this).find('.entry-post').first().children().first().text();
+            articleJson.datePosted = $(this).find('.posted-on').first().first().text();
+            articleJson.author = $(this).find('.byline').first().first().text();
+            articleJson.imageUrl = $(this).find('.entry-post').children().first().children().first().attr('src');
+            console.log($(this).find('.entry-post').children().first().children().first().attr('src'));
+
+          });
+          blogsJson.articles.push(articleJson);
+        });
+
+      });
+      //sent articles
+      res.send(JSON.stringify(blogsJson));
+    }
+  });
+
+});
+
+module.exports = router;
---
+++ routes/blogs.js
@@ -0,0 +1,71 @@
+//
+//  blogs.js
+//  kdicAPI
+//
+//  Created by Shaun Mataire on 1/14/16.
+//  Copyright © 2016 AppDev. All rights reserved.
+//
+
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var cheerio = require('cheerio');
+
+/* GET blogs home. */
+router.get('/', function(req, res, next) {
+
+  var blogsUrl = 'http://kdic.grinnell.edu'
+
+  request(blogsUrl, function(error, response, html){
+    //check whether there was an error making the request
+    if(!error){
+      //get the cheerio html object
+      var $ = cheerio.load(html);
+
+      //json result = articles object that is an array of json objects
+      var blogsJson = {
+        "articles": []
+      }
+
+      //we'' use the unique header as a starting point
+      $('div .posts-layout').filter(function(){
+
+        //get the html object data
+        var data = $(this)
+
+        //loop through articles
+        data.children().each(function(){
+
+          //varticle objects
+          var articleTitle, fullArticleUrl, articleText, datePosted, author
+
+          //artcle object
+          var articleJson = {
+            articleTitle: "",
+            fullArticleUrl: "",
+            articleText: "",
+            datePosted: "",
+            author: ""
+          }
+
+          $(this).each(function(){
+
+            articleJson.articleTitle = $(this).find('.entry-header').first().children().first().text();
+            articleJson.fullArticleUrl = 'nil';
+            articleJson.articleText = $(this).find('.entry-post').first().children().first().text();
+            articleJson.datePosted = $(this).find('.posted-on').first().first().text();
+            articleJson.author = $(this).find('.byline').first().first().text();
+
+          });
+          blogsJson.articles.push(articleJson);
+        });
+
+      });
+      //sent articles
+      res.send(JSON.stringify(blogsJson));
+    }
+  });
+
+});
+
+module.exports = router;
